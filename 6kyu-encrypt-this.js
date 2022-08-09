ca1070e60a25bccd144064491b09ac409bb4f80f
Paul Cabezal <paulcabezal@gmail.com>
2022-08-09 17:11:47 -0400

    7, 6, 5

--- 6kyu-encrypt-this.js
+++ 6kyu-encrypt-this.js
@@ -1,6 +1,6 @@
 var encryptThis = function(texts) {
-
     arr = texts.split(' ');
+
     newArr = arr.map(text => {
         ascii = text.charCodeAt(0);
         if(text.length > 2) {
@@ -15,19 +15,4 @@ var encryptThis = function(texts) {
     })
 
     return newArr.join(' ')
-
-    // ascii = text.charCodeAt(0);
-    // if(text.length > 2) {
-    //     word = text.slice(1)
-    //     newWord = ascii + word.slice(-1) + word.slice(1, -1) + word[0]
-    // } else if (text.length == 2) {
-    //     newWord = ascii + text[1]
-    // } else {
-    //     newWord = ascii
-    // }
-    // return newWord
 }
-
-console.log(encryptThis("Hello"));
-console.log(encryptThis("good"));
-console.log(encryptThis("hello world"));
