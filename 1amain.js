1c6cd961c53d617c3f0dc1e1cea6a937a5acbe46
Paul Cabezal <paulcabezal@gmail.com>
2022-09-02 12:33:45 -0400

    6

--- 1amain.js
+++ 1amain.js
@@ -1,4 +1,9 @@
-function createPhoneNumber(n){
-  return `(${n[0]}${n[1]}${n[2]})-${n[3]}${n[4]}${n[5]}-
-            ${n[6]}${n[7]}${n[8]}${n[9]}`
+function solution(number){
+  let arr = [];
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) arr.push(i)
+  }
+  return arr.reduce((p,c) => p + c)
 }
+
+console.log(solution(10));
