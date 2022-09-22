ddf54570d86ab2a710fca8bdb008747cb9972e86
Paul Cabezal <paulcabezal@gmail.com>
2022-09-22 17:26:25 -0400

    easy

--- 1amain.js
+++ 1amain.js
@@ -1,5 +1,5 @@
 function getParticipants(shakes){
-  if (shakes === 0) return 0;
+  if (shakes === 0) return 00;
   for (let people = 2; people <= shakes+1; people++) {
     if (((people*(people - 1)) / 2) >= shakes) return people;
   };
