baad472bab2fe5f206bebeda4a1c0e15c73b73fd
Paul Cabezal <paulcabezal@gmail.com>
2022-11-15 17:44:12 -0500

    6

--- 6kyu-sort-the-odd.js
+++ 6kyu-sort-the-odd.js
@@ -10,3 +10,19 @@ function sortArray(array) {
     }});
   return swapped;
 }
+
+// v2.0
+function sortArray(array) {
+  // filter out even numbers, then sort odd numbers in ascending order
+   let sorted = array.filter(x => x%2 !== 0).sort((a,b) => a - b);
+   
+   // loop through original array replacing odd numbers with sorted values
+   let pointer = 0;
+   for (let i = 0; i < array.length; i++) {
+     if (array[i] % 2 !== 0) {
+       array[i] = sorted[pointer];
+       pointer++;
+     }
+   }
+   return array
+ }
